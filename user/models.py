9d015183918ecd1ae810d5fe4e11bfab4ef9a364
coldbruk84 <coldbruk84@gmail.com>
2020-10-05 11:11:50 +0900

    20201005 kwanho commit

--- user/models.py
+++ user/models.py
@@ -7,6 +7,8 @@ class SgxUser(models.Model):
     username = models.CharField(max_length=128, verbose_name='사용자명')
     useremail = models.EmailField(max_length=128, verbose_name='사용자이메일')
     password = models.CharField(max_length=128, verbose_name='비밀번호')
+    auth = models.CharField(max_length=128, verbose_name='권한', default='user')
+    use_state = models.CharField(max_length=128, verbose_name='등록상태', default='wait')
     registered_dt = models.DateField(auto_now_add=True, verbose_name='등록시간')
 
     def __str__(self):
